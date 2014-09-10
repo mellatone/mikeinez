833465ab102012177a269d752130292e8f52ceab
Steven Valladolid <steven@hazeltree.org>
2014-09-09 19:44:45 -0700

    Latest design changes

--- app.js
+++ app.js
@@ -1,50 +1,61 @@
 App = Ember.Application.create();
 
 App.ApplicationAdapter = DS.RESTAdapter.extend({
-  // Application specific overrides go here
   host: "http://api.mikeandinez.com"
 });
 
 App.ApplicationView = Ember.View.extend({
-  classNames: ['app']
-});
-
-Ember.RadioButton = Ember.View.extend({  
-  tagName : "input",
-  type : "radio",
-  attributeBindings : [ "name", "type", "value", "checked:checked:" ],
-  click : function() {
-      this.set("selection", this.$().val())
-  },
-  checked : function() {
-      return this.get("value") == this.get("selection");   
-  }.property()
+  elementId: 'app'
 });
 
 App.Router.map(function() {
-  // put your routes here
+  this.route('thanks');
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return this.store.createRecord('rsvp', {
-      first: 'Johnny',
-      last: 'Appledseed',
-      email: 'jaseed@yahoo.com',
-      note: 'Hi, congratulations!',
-      ceremony: true,
-      reception: true,
-      guests: 10
-    });
+    return this.store.createRecord('rsvp');
   }
 });
-
 App.Rsvp = DS.Model.extend({
   first: DS.attr('string'),
   last: DS.attr('string'),
   email: DS.attr('string'),
   note: DS.attr('string'),
-  ceremony: DS.attr('boolean'),
-  reception: DS.attr('boolean'),
-  guests: DS.attr('number')
+  ceremony: DS.attr('number', {defaultValue: 0}),
+  reception: DS.attr('number', {defaultValue: 0}),
+  isAttending: DS.attr('boolean')
+});
+App.IndexController = Ember.ObjectController.extend({
+  isNotAttending: false,
+  ceremonyOnly: 0,
+  receptionOnly: 0,
+  ceremonyAndReception: 0,
+  ceremonyCounter: function() {
+    var count = parseInt(this.get('ceremonyOnly')) + parseInt(this.get('ceremonyAndReception'));
+    this.set('ceremony', count);
+  }.observes('ceremonyOnly', 'ceremonyAndReception'),
+  receptionCounter: function() {
+    var count = parseInt(this.get('receptionOnly')) + parseInt(this.get('ceremonyAndReception'));
+    this.set('reception', count);
+  }.observes('receptionOnly', 'ceremonyAndReception'),
+  actions: {
+    willAttend: function(yes) {
+      var isAttending = (yes) ? true : false,
+          isNotAttending = (yes) ? false : true;
+      this.setProperties({
+        isAttending: isAttending,
+        isNotAttending: isNotAttending,
+        ceremony: 0,
+        reception: 0
+      });
+    },
+    sendConfirmation: function() {
+      this.transitionToRoute('thanks');
+    }
+  }
+});
+App.ThanksController = Ember.Controller.extend({
+  needs: 'index',
+  isAttendingBinding: 'controllers.index.content.isAttending'
 });
